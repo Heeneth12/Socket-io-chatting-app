57de58e1755bb5c4d64cc69ed9b2468e0d65a9fc
heeneth <heeneth123@gmail.com>
2023-12-26 16:27:26 +0530

    Your commit message here

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+import Body from "../Components/Body";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <Body />
+    </>
+  );
+}
+
+export default Home;
